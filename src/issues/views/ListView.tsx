c1e23158a41ad1012bf4790d056cf206eed5883d
Joletuar <joletuar@gmail.com>
2023-07-17 21:35:59 -0500

    Inicio de infinite scroll

--- src/issues/views/ListView.tsx
+++ src/issues/views/ListView.tsx
@@ -11,7 +11,7 @@ export const ListView = () => {
   const [selectedLabels, setSelectedLabels] = useState<string[]>([]);
   const [estado, setEstado] = useState<State>();
 
-  const { issuesQuery, page, nextPage, prevPage } = useIssues({
+  const { issuesQuery, page } = useIssues({
     state: estado,
     labels: selectedLabels,
   });
